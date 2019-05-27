757b3a9aac56adc95b481270a00c57a4f1dbc68d
unknown <cice@A5P04.CICE.LOCAL>
2019-05-27 09:52:57 +0200

    modificacion

--- src/java/tk/anitoys/registrador/SeleccionFamiliaServlet.java
+++ src/java/tk/anitoys/registrador/SeleccionFamiliaServlet.java
@@ -1,46 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tk.anitoys.registrador;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author cice
- */
 public class SeleccionFamiliaServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet SeleccionFamiliaServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet SeleccionFamiliaServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+
+        int id_familia = Integer.parseInt(request.getParameter("id_familia"));
+        try {
+            System.out.println("1");
+            GestorSeleccionFamilia gsf = new GestorSeleccionFamilia();
+            System.out.println("2");
+            ResultSet producto = gsf.getFamilia(id_familia);
+            System.out.println("3");
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/seleccionFamilia.jsp");
+            System.out.println("4");
+            request.setAttribute("datos", producto);
+            System.out.println("5");
+            dispatcher.forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 

c5da8ddf7f4b7c4c76887f71dedac7a5e93d5e55
unknown <cice@A5P04.CICE.LOCAL>
2019-05-27 09:53:04 +0200

    modificacion

--- src/java/tk/anitoys/registrador/GestorBBDD.java
+++ src/java/tk/anitoys/registrador/GestorBBDD.java
@@ -8,7 +8,7 @@ public class GestorBBDD {
     protected Connection conexion;
     public GestorBBDD () throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
-        conexion = DriverManager.getConnection ("jdbc:mysql://localhost:8888/anitoys","Fabiola","fabiola");
+        conexion = DriverManager.getConnection ("jdbc:mysql://localhost:8889/anitoys","Fabiola","fabiola");
     }
 public void cerrarConexion()throws SQLException{
     conexion.close();

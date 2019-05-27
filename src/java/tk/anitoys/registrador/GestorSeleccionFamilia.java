4857c781519dbeb98608af4abba3f1ed4f53e904
unknown <cice@A5P04.CICE.LOCAL>
2019-05-27 09:52:25 +0200

    modificacion

--- src/java/tk/anitoys/registrador/GestorSeleccionFamilia.java
+++ src/java/tk/anitoys/registrador/GestorSeleccionFamilia.java
@@ -1,19 +1,20 @@
-
 package tk.anitoys.registrador;
+
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
-public class GestorSeleccionFamilia extends GestorBBDD{
-    public GestorSeleccionFamilia() throws ClassNotFoundException, SQLException{
-    super();
+public class GestorSeleccionFamilia extends GestorBBDD {
+
+    public GestorSeleccionFamilia() throws ClassNotFoundException, SQLException {
+        super();
     }
-public ResultSet getFamilia() throws SQLException{
-        String sql = "SELECT * FROM productos where familia=";
-        Statement sentencia = conexion.createStatement ();
-        ResultSet resultado = sentencia.executeQuery(sql);
-        return resultado;
-        
-        
+
+    public ResultSet getFamilia(int id_familia) throws SQLException {
+        String sql = "SELECT * FROM producto where id_familia=?";
+        PreparedStatement ps = conexion.prepareStatement(sql);
+        ps.setInt(1, id_familia);
+        ResultSet rs = ps.executeQuery();
+        return rs;
     }
 }
